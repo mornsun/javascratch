9be281f54706d36e6d62a1eed1575c3b266a5814
chaunceychen <chaunceychan@gmail.com>
2015-08-07 00:44:32 +0800

    update

---
+++ src/topcoder/ImplementstrStr.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package topcoder;
+
+/**
+ * @author cherry
+ *
+ */
+public class ImplementstrStr
+{
+    public static int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) return -1;
+        return haystack.indexOf(needle);
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println(strStr("hello world", "x"));
+	}
+
+}
---
+++ src/topcoder/SearchInsertPosition.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package topcoder;
+
+/**
+ * @author cherry
+ *
+ */
+public class SearchInsertPosition
+{
+    public static int searchInsert(int[] nums, int target) {
+    	if (nums == null || nums.length == 0) return 0;
+        int lo = 0;
+        int hi = nums.length - 1;
+        if (nums[hi] < target) return hi+1;
+        while (lo < hi) {
+        	int m = lo + ((hi-lo)>>1);
+        	if (nums[m] == target) {
+        		return m;
+        	} else if (nums[m] < target) {
+        		lo = m + 1;
+        	} else {
+        		hi = m;
+        	}
+        }
+        return lo;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println(searchInsert(new int[]{1,3,5,6}, 0));
+	}
+
+}
