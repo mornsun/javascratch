28e7caca3db5d3878e031c4df3bbc234014b59ec
chaunceychen <chaunceychan@gmail.com>
2015-08-16 00:19:34 +0800

    update

---
+++ src/topcoder.py/LargestRectangleinHistogram.py
@@ -0,0 +1,53 @@
+'''
+
+@author: Chauncey
+'''
+import sys
+import collections
+
+class Solution:
+    # @param {integer[]} height
+    # @return {integer}
+    def largestRectangleArea(self, height):
+        if height is None or len(height) == 0: return 0
+        height.append(0)
+        l = len(height)
+        stack = []
+        max_area = 0
+        i = 0
+        while i < l:
+            if len(stack) == 0 or height[i] > height[stack[-1]]:
+                stack.append(i)
+                i += 1
+            else:
+                tmp = stack.pop()
+                max_area = max(max_area, height[tmp]*(i if len(stack)==0 else i-stack[-1]-1))
+        return max_area
+        
+        
+    def largestRectangleArea1(self, height):
+        if height is None or len(height) == 0: return 0
+        l = len(height)
+        max_area = -sys.maxsize-1
+        for i in xrange(l):
+            dp = height[i]
+            area = i;
+            #print i,0,dp[i][0],area
+            if area > max_area:
+                max_area = area
+            for j in xrange(1,l-i):
+                dp = min(dp, height[i+j])
+                area = (j+1)*dp;
+                #print i,j,dp[i][j],area
+                if area > max_area:
+                    max_area = area
+        return max_area
+         
+            
+                    
+if __name__ == '__main__':
+    solution = Solution();
+    print solution.largestRectangleArea([2,1,5,6,2,3])
+    
+    
+    
---
+++ src/topcoder.py/MaximalRectangle.py
@@ -0,0 +1,45 @@
+'''
+
+@author: Chauncey
+'''
+import sys
+import collections
+
+class Solution:
+    # @param {character[][]} matrix
+    # @return {integer}
+    def maximalRectangle(self, matrix):
+        if matrix is None or len(matrix) == 0 or matrix[0] is None or len(matrix[0]) == 0: return 0
+        max_area = 0
+        m = len(matrix)
+        n = len(matrix[0])
+        l = [0 for i in xrange(n)]
+        r = [n for i in xrange(n)]
+        h = [0 for i in xrange(n)]
+        for i in xrange(m):
+            left = 0
+            for j in xrange(n):
+                if matrix[i][j] == '0':
+                    left = j+1
+                    h[j] = 0
+                    l[j] = 0
+                    r[j] = n
+                else:
+                    l[j] = max(l[j], left)
+                    h[j] += 1
+            right = n
+            for j in xrange(n-1,-1,-1):
+                if matrix[i][j] == '0':
+                    right = j
+                else:
+                    r[j] = min(r[j], right)
+                    max_area = max(max_area, (r[j]-l[j])*h[j])
+                    #print r[j], l[j], h[j]
+        return max_area
+                    
+if __name__ == '__main__':
+    solution = Solution();
+    print solution.maximalRectangle(["00110",'11111','01110','00110'])
+    
+    
+    
