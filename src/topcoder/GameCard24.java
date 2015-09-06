ffd164816e5018c76d3d446fed59b91b3f8b9533
mornsun <eterne@126.com>
2015-09-07 00:04:51 +0800

    update

--- src/topcoder/GameCard24.java
+++ src/topcoder/GameCard24.java
@@ -10,35 +10,35 @@ import java.util.*;
  */
 public class GameCard24
 {
-	/*private final class SubEvaluation {
-		public int answer;
+	private final class SubEvaluation {
+		public char operator;
 		public String expression;
-		public SubEvaluation(int a, String exp) {answer=a; expression=exp;}
-	}*/
+		public SubEvaluation(char op, String exp) {operator=op; expression=exp;}
+	}
 	public static final int MAX_LENGTH = 32;
     public List<String> card24(int[] cards) {
     	if (cards == null || cards.length==0 || cards.length>MAX_LENGTH) return null;
     	List<String> res = new ArrayList<String>();
     	int set_number = 1 << cards.length;
-    	ArrayList<HashMap<Float, String>> sets = new ArrayList<HashMap<Float, String>>(set_number);
+    	ArrayList<HashMap<Float, SubEvaluation>> sets = new ArrayList<HashMap<Float, SubEvaluation>>(set_number);
     	for (int i=0; i<set_number; ++i) {
-    		sets.add(new HashMap<Float, String>());
+    		sets.add(new HashMap<Float, SubEvaluation>());
     	}
     	for (int i=0; i<cards.length; ++i) {
-    		sets.get(1<<i).put((float)cards[i], String.valueOf(cards[i]));
+    		sets.get(1<<i).put((float)cards[i], new SubEvaluation('#', String.valueOf(cards[i])));
     	}
     	for (int i=1; i<set_number; ++i) {
     		evaluate(sets, i);
     	}
-    	for (Map.Entry<Float, String> entry: sets.get(set_number-1).entrySet()) {
+    	for (Map.Entry<Float, SubEvaluation> entry: sets.get(set_number-1).entrySet()) {
     		float ans = entry.getKey();
     		if (ans+0.000001 < 24 || ans-0.000001 > 24) continue;
-    		res.add(entry.getValue());
+    		res.add(entry.getValue().expression);
     	}
     	return res;
     }
     
-    private final void evaluate(ArrayList<HashMap<Float, String>> sets, int index) {
+    private final void evaluate(ArrayList<HashMap<Float, SubEvaluation>> sets, int index) {
     	int cnt = 0;
     	int[] pos = new int[MAX_LENGTH];
     	for (int test=index; test!=0; ++cnt) {
@@ -63,33 +63,56 @@ public class GameCard24
     	}
     }
     
-    private final void evaluate(ArrayList<HashMap<Float, String>> sets, int index, int left, int right) {
-    	HashMap<Float, String> curset = sets.get(index);
-    	for (Map.Entry<Float, String> entryleft: sets.get(left).entrySet()) {
-    		for (Map.Entry<Float, String> entryright: sets.get(right).entrySet()) {
+    private final void evaluate(ArrayList<HashMap<Float, SubEvaluation>> sets, int index, int left, int right) {
+    	HashMap<Float, SubEvaluation> curset = sets.get(index);
+    	for (Map.Entry<Float, SubEvaluation> entryleft: sets.get(left).entrySet()) {
+    		for (Map.Entry<Float, SubEvaluation> entryright: sets.get(right).entrySet()) {
+    			//avoid redundant brackets 
     			float l = entryleft.getKey();
     			float r = entryright.getKey();
     			float ans = l + r;
-    			if (curset.get(ans) == null)
-    				curset.put(ans, '('+entryleft.getValue()+'+'+entryright.getValue()+')');
+    			if (curset.get(ans) == null) {
+    				curset.put(ans, new SubEvaluation('+', entryleft.getValue().expression+'+'+entryright.getValue().expression));
+    			}
     			ans = l * r;
-    			if (curset.get(ans) == null)
-    				curset.put(ans, '('+entryleft.getValue()+'*'+entryright.getValue()+')');
+    			if (curset.get(ans) == null) {
+    				char lop = entryleft.getValue().operator;
+    				String lexp = (lop=='+' || lop=='-') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
+    				char rop = entryright.getValue().operator;
+    				String rexp = (rop!='*' && rop!='#') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
+    				curset.put(ans, new SubEvaluation('*', lexp+'*'+rexp));
+    			}
     			ans = l - r;
-    			if (curset.get(ans) == null)
-    				curset.put(ans, '('+entryleft.getValue()+'-'+entryright.getValue()+')');
+    			if (curset.get(ans) == null) {
+    				char rop = entryright.getValue().operator;
+    				String rexp = (rop=='+' || rop=='-') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
+    				curset.put(ans, new SubEvaluation('-', entryleft.getValue().expression+'-'+rexp));
+    			}
     			ans = r - l;
-    			if (curset.get(ans) == null)
-    				curset.put(ans, '('+entryright.getValue()+'-'+entryleft.getValue()+')');
+    			if (curset.get(ans) == null) {
+    				char rop = entryleft.getValue().operator;
+    				String rexp = (rop=='+' || rop=='-') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
+    				curset.put(ans, new SubEvaluation('-', entryright.getValue().expression+'-'+rexp));
+    			}
     			if (r != 0) {
 	    			ans = l / r;
-	    			if (curset.get(ans) == null)
-	    				curset.put(ans, '('+entryleft.getValue()+'/'+entryright.getValue()+')');
+	    			if (curset.get(ans) == null) {
+	    				char lop = entryleft.getValue().operator;
+	    				String lexp = (lop=='+' || lop=='-') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
+	    				char rop = entryright.getValue().operator;
+	    				String rexp = (rop!='#') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
+	    				curset.put(ans, new SubEvaluation('/', lexp+'/'+rexp));
+	    			}
     			}
     			if (l != 0) {
 	    			ans = r / l;
-	    			if (curset.get(ans) == null)
-	    				curset.put(ans, '('+entryright.getValue()+'/'+entryleft.getValue()+')');
+	    			if (curset.get(ans) == null) {
+	    				char lop = entryright.getValue().operator;
+	    				String lexp = (lop=='+' || lop=='-') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
+	    				char rop = entryleft.getValue().operator;
+	    				String rexp = (rop!='#') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
+	    				curset.put(ans, new SubEvaluation('/', lexp+'/'+rexp));
+	    			}
     			}
     		}
     	}
