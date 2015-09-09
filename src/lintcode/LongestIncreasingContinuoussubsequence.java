1aa3d70d02d3135a9c7ab18e90b316aa82ddf837
mornsun <eterne@126.com>
2015-09-09 22:37:45 +0800

    update

---
+++ src/lintcode/LongestIncreasingContinuoussubsequence.java
@@ -0,0 +1,71 @@
+package lintcode;
+
+import java.util.*;
+
+import topcoder.BinarySearchTreeIterator.TreeNode;
+import topcoder.LinkedListCycle.ListNode;
+
+/**
+ * Give you an integer array (index from 0 to n-1, where n is the size of this array)，find the longest increasing continuous subsequence in this array. (The definition of the longest increasing continuous subsequence here can be from right to left or from left to right)
+
+Have you met this question in a real interview? Yes
+Example
+For [5, 4, 2, 1, 3], the LICS is [5, 4, 2, 1], return 4.
+
+For [5, 1, 2, 3, 4], the LICS is [1, 2, 3, 4], return 4.
+
+Note
+O(n) time and O(1) extra space.
+
+Tags Expand 
+Dynamic Programming Array
+
+
+Related Problems Expand 
+Hard Longest Increasing Continuous subsequence II 21 %
+
+ * @author Chauncey
+ *
+ */
+public class LongestIncreasingContinuoussubsequence
+{
+    /**
+     * @param A an array of Integer
+     * @return  an integer
+     */
+    public int longestIncreasingContinuousSubsequence(int[] A) {
+        if (A==null || A.length==0) return 0;
+        int lo = 0;
+        boolean asc = false;
+        int max = 1;
+        for (int i=1; i<A.length; ++i) {
+        	if (A[i] >= A[i-1] && asc == false) {
+        		if (i-lo > max) max = i-lo;
+        		asc = true;
+        		lo = i - 1;
+        	} else if (A[i] <= A[i-1] && asc == true) {
+        		if (i-lo > max) max = i-lo;
+        		asc = false;
+        		lo = i - 1;
+        	}
+        }
+		if (A.length-lo > max) max = A.length-lo;
+		return max;
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		LongestIncreasingContinuoussubsequence solution = new LongestIncreasingContinuoussubsequence();
+
+		//4
+		System.out.println(solution.longestIncreasingContinuousSubsequence(new int[]{5, 4, 2, 1, 3}));
+		//4
+		System.out.println(solution.longestIncreasingContinuousSubsequence(new int[]{5, 1, 2, 3, 4}));
+		//4
+		System.out.println(solution.longestIncreasingContinuousSubsequence(new int[]{5, 5, 4, 2, 1, 3}));
+	}
+
+}
---
+++ src/lintcode/LongestIncreasingContinuoussubsequenceII.java
@@ -0,0 +1,119 @@
+package lintcode;
+
+import java.util.*;
+
+import topcoder.BinarySearchTreeIterator.TreeNode;
+import topcoder.LinkedListCycle.ListNode;
+
+/**
+ * Give you an integer matrix (with row size n, column size m)，find the longest increasing continuous subsequence in this matrix. (The definition of the longest increasing continuous subsequence here can start at any row or column and go up/down/right/left any direction).
+
+Have you met this question in a real interview? Yes
+Example
+Given a matrix:
+
+[
+  [1 ,2 ,3 ,4 ,5],
+  [16,17,24,23,6],
+  [15,18,25,22,7],
+  [14,19,20,21,8],
+  [13,12,11,10,9]
+]
+return 25
+
+Challenge
+O(nm) time and memory.
+
+Tags Expand 
+Dynamic Programming
+
+
+Related Problems Expand 
+Easy Longest Increasing Continuous subsequence 26 %
+
+ * @author Chauncey
+ *
+ */
+public class LongestIncreasingContinuoussubsequenceII
+{
+    /**
+     * @param A an integer matrix
+     * @return  an integer
+     */
+    public int longestIncreasingContinuousSubsequenceII(int[][] A) {
+        if (A==null || A.length==0 || A[0].length==0) return 0;
+        int m = A.length;
+        int n = A[0].length;
+        int max = 0;
+        //boolean[][][] asc = new boolean[m][n][4];
+        int[][][] step = new int[m][n][4]; //up, down, left, right
+        for (int i=0; i<m; ++i) {
+        	for (int j=0; j<n; ++j) {
+        		max = Math.max(max, dfs(A, step, i, j, false));
+        		max = Math.max(max, dfs(A, step, i, j, true));
+        	}
+        }
+        return max+1;
+    }
+    
+    private final int dfs(int[][] A, int[][][] step, int i, int j, boolean asc) {
+    	int max = 0;
+    	if (i > 0 && A[i][j]!=A[i-1][j] && A[i][j]<A[i-1][j] == asc) { //up
+	    	if (step[i][j][0] == 0) {
+	    		step[i][j][0] = dfs(A, step, i-1, j, asc) + 1;
+	    	}
+	    	max = step[i][j][0];
+    	}
+    	if (i < A.length-1 && A[i][j]!=A[i+1][j] && A[i][j]<A[i+1][j] == asc) { //down
+	    	if (step[i][j][1] == 0) {
+	    		step[i][j][1] = dfs(A, step, i+1, j, asc) + 1;
+	    	}
+	    	max = Math.max(max, step[i][j][1]);
+    	}
+    	if (j > 0 && A[i][j]!=A[i][j-1] && A[i][j]<A[i][j-1] == asc) { //left
+	    	if (step[i][j][2] == 0) {
+	    		step[i][j][2] = dfs(A, step, i, j-1, asc) + 1;
+	    	}
+	    	max = Math.max(max, step[i][j][2]);
+    	}
+    	if (j < A[0].length-1 && A[i][j]!=A[i][j+1] && A[i][j]<A[i][j+1] == asc) { //down
+	    	if (step[i][j][3] == 0) {
+	    		step[i][j][3] = dfs(A, step, i, j+1, asc) + 1;
+	    	}
+	    	max = Math.max(max, step[i][j][3]);
+    	}
+    	return max;
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		LongestIncreasingContinuoussubsequenceII solution = new LongestIncreasingContinuoussubsequenceII();
+
+		int[][] matrix = new int[][]{
+				  {1 ,2 ,3 ,4 ,5},
+				  {16,17,24,23,6},
+				  {15,18,25,22,7},
+				  {14,19,20,21,8},
+				  {13,12,11,10,9}};
+		//25
+		System.out.println(solution.longestIncreasingContinuousSubsequenceII(matrix));
+		matrix = new int[][]{
+				  {13,12,11,10,9},
+				  {14,19,20,21,8},
+				  {15,18,25,22,7},
+				  {16,17,24,23,6},
+				  {1 ,2 ,3 ,4 ,5}};
+		//25
+		System.out.println(solution.longestIncreasingContinuousSubsequenceII(matrix));
+		matrix = new int[][]{
+				  {1,1,1},
+				  {1,1,1},
+				  {1,1,1}};
+		//25
+		System.out.println(solution.longestIncreasingContinuousSubsequenceII(matrix));
+	}
+
+}
