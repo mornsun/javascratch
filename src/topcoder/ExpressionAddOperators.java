19b6e456fe4e31dbada5f9d06f893128fa0f56c6
mornsun <eterne@126.com>
2015-09-27 18:27:09 +0800

    update

--- src/topcoder/ExpressionAddOperators.java
+++ src/topcoder/ExpressionAddOperators.java
@@ -25,125 +25,111 @@ public class ExpressionAddOperators
 	private final class SubEvaluation {
 		public char operator;
 		public String expression;
-		public SubEvaluation(char op, String exp) {operator=op; expression=exp;}
+		public int val;
+		public SubEvaluation(int v, char op, String exp) {val=v; operator=op; expression=exp;}
+		public String toString() { return "[" + expression + "]"; }
+		@Override
+		public int hashCode() {
+			return expression.hashCode();
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			SubEvaluation other = (SubEvaluation) obj;
+			if (val != other.val)
+				return false;
+			if (expression == null) {
+				if (other.expression != null)
+					return false;
+			} else if (!expression.equals(other.expression))
+				return false;
+			return true;
+		}
 	}
-	public static final int MAX_LENGTH = 32;
-    public List<String> card24(int[] cards) {
-    	if (cards == null || cards.length==0 || cards.length>MAX_LENGTH) return null;
+	
+    public List<String> addOperators(String num, int target) {
     	List<String> res = new ArrayList<String>();
-    	int set_number = 1 << cards.length;
-    	ArrayList<HashMap<Float, SubEvaluation>> sets = new ArrayList<HashMap<Float, SubEvaluation>>(set_number);
-    	for (int i=0; i<set_number; ++i) {
-    		sets.add(new HashMap<Float, SubEvaluation>());
+    	if (num == null || num.length()==0) return res;
+    	int[] nums = new int[num.length()];
+    	for (int i=0; i<nums.length; ++i) {
+    		nums[i] = num.charAt(i) - '0';
     	}
-    	for (int i=0; i<cards.length; ++i) {
-    		sets.get(1<<i).put((float)cards[i], new SubEvaluation('#', String.valueOf(cards[i])));
+    	ArrayList<ArrayList<HashSet<SubEvaluation>>> dp = new ArrayList<ArrayList<HashSet<SubEvaluation>>>();
+    	for (int d=0; d<nums.length; ++d) {
+    		ArrayList<HashSet<SubEvaluation>> ddp = new ArrayList<HashSet<SubEvaluation>>();
+    		for (int s=0; s+d<nums.length; ++s) {
+    			ddp.add(new HashSet<SubEvaluation>());
+    		}
+    		dp.add(ddp);
     	}
-    	for (int i=1; i<set_number; ++i) {
-    		evaluate(sets, i);
+    	for (int d=0; d<nums.length; ++d) {
+    		for (int s=0; s+d<nums.length; ++s) {
+    			if (d == 0) {
+            		dp.get(d).get(s).add(new SubEvaluation(nums[s], '#', String.valueOf(nums[s])));
+    			} else if (d <= 10 && nums[s]>0) {
+	    			long v = nums[s];
+	    			for (int dd=1; dd<=d; ++dd) {
+	    				v = v*10+nums[s+dd];
+	    			}
+	    			if (v>0 && v<=Integer.MAX_VALUE)
+	            		dp.get(d).get(s).add(new SubEvaluation((int)v, '#', num.substring(s, s+d+1)));
+    			}
+    			int lmax = 1;
+    			if (s>0) {
+    				lmax = nums[0];
+        			for (int i=1; i<s; ++i) {
+        				lmax = lmax*10+nums[i];
+        			}
+    			}
+    			if (lmax==0) lmax = 1;
+    			int rmax = 1;
+    			if (s+d+1<nums.length) {
+	    			rmax = nums[s+d+1];
+	    			for (int i=s+d+2; i<nums.length; ++i) {
+	    				rmax = rmax*10+nums[i];
+	    			}
+    			}
+    			if (rmax==0) rmax = 1;
+    			for (int dd=0; dd<d; ++dd) {
+    				evaluate(dp, s, s+dd, s+d, target, lmax, rmax);
+    			}
+    		}
     	}
-    	for (Map.Entry<Float, SubEvaluation> entry: sets.get(set_number-1).entrySet()) {
-    		float ans = entry.getKey();
-    		if (ans+0.000001 < 24 || ans-0.000001 > 24) continue;
-    		res.add(entry.getValue().expression);
+    	for (SubEvaluation eval : dp.get(nums.length-1).get(0)) {
+    		if (eval.val == target) {
+    			res.add(eval.expression);
+    		}
     	}
     	return res;
     }
-
-    private final void evaluate(ArrayList<HashMap<Float, SubEvaluation>> sets, int index) {
-    	int cnt = 0;
-    	int[] pos = new int[MAX_LENGTH];
-    	for (int popcount=index; popcount!=0; ++cnt) {
-    		int prev = popcount;
-    		popcount &= (popcount-1);
-    		pos[cnt] = prev ^ popcount;
-    	}
-    	for (int i=(1<<(cnt-1))-1; i>0; --i) {
-    		int left = 0;
-    		int bittest = i;
-    		for (int j=0; j<cnt; ++j, bittest>>=1) {
-    			if ( (bittest&1) != 0) left |= pos[j];
-    		}
-    		int right = index ^ left;
-    		evaluate(sets, index, left, right);
-    	}
-    	
-    }
-    private final void evaluate1(ArrayList<HashMap<Float, SubEvaluation>> sets, int index) {
-    	int cnt = 0;
-    	int[] pos = new int[MAX_LENGTH];
-    	for (int test=index; test!=0; ++cnt) {
-    		int prev = test;
-    		test &= (test-1);
-    		pos[cnt] = prev ^ test;
-    	}
-    	int combination_num = 1<<(cnt-1);
-    	for (int i=combination_num-1; i>0; --i) {
-    		int left = 0;
-    		int k = 0;
-    		int test = i;
-    		while (test != 0) {
-    			if ((test&1) == 1) {
-    				left |= pos[k];
+    private void evaluate(ArrayList<ArrayList<HashSet<SubEvaluation>>> dp, int s, int k, int e, int target, int lmax, int rmax) {
+    	HashSet<SubEvaluation> curr = dp.get(e-s).get(s);
+    	for (SubEvaluation left : dp.get(k-s).get(s)) {
+    		for (SubEvaluation right : dp.get(e-k-1).get(k+1)) {
+    			//1+0-0+0+1
+    			long v = left.val + right.val;
+    			long max = lmax + (v>0?v:-v) + rmax;
+    			if (v<=Integer.MAX_VALUE && max>=target && -max<=target)
+    				curr.add(new SubEvaluation((int)v, '+', left.expression+'+'+right.expression));
+    			if (right.operator=='*'||right.operator=='#') {
+    				v = left.val-right.val;
+        			max = lmax + (v>0?v:-v) + rmax;
+        			if (v<=Integer.MAX_VALUE && max>=target && -max<=target)
+        				curr.add(new SubEvaluation(left.val-right.val, '-', left.expression+'-'+right.expression));
     			}
-    			test >>= 1;
-    			++k;
-    		}
-    		int right = index ^ left;
-    		evaluate(sets, index, left, right);
-    	}
-    }
-    
-    private final void evaluate(ArrayList<HashMap<Float, SubEvaluation>> sets, int index, int left, int right) {
-    	HashMap<Float, SubEvaluation> curset = sets.get(index);
-    	for (Map.Entry<Float, SubEvaluation> entryleft: sets.get(left).entrySet()) {
-    		for (Map.Entry<Float, SubEvaluation> entryright: sets.get(right).entrySet()) {
-    			//avoid redundant brackets 
-    			float l = entryleft.getKey();
-    			float r = entryright.getKey();
-    			float ans = l + r;
-    			if (curset.get(ans) == null) {
-    				curset.put(ans, new SubEvaluation('+', entryleft.getValue().expression+'+'+entryright.getValue().expression));
-    			}
-    			ans = l * r;
-    			if (curset.get(ans) == null) {
-    				char lop = entryleft.getValue().operator;
-    				String lexp = (lop=='+' || lop=='-') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
-    				char rop = entryright.getValue().operator;
-    				String rexp = (rop!='*' && rop!='#') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
-    				curset.put(ans, new SubEvaluation('*', lexp+'*'+rexp));
-    			}
-    			ans = l - r;
-    			if (curset.get(ans) == null) {
-    				char rop = entryright.getValue().operator;
-    				String rexp = (rop=='+' || rop=='-') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
-    				curset.put(ans, new SubEvaluation('-', entryleft.getValue().expression+'-'+rexp));
-    			}
-    			ans = r - l;
-    			if (curset.get(ans) == null) {
-    				char rop = entryleft.getValue().operator;
-    				String rexp = (rop=='+' || rop=='-') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
-    				curset.put(ans, new SubEvaluation('-', entryright.getValue().expression+'-'+rexp));
-    			}
-    			if (r != 0) {
-	    			ans = l / r;
-	    			if (curset.get(ans) == null) {
-	    				char lop = entryleft.getValue().operator;
-	    				String lexp = (lop=='+' || lop=='-') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
-	    				char rop = entryright.getValue().operator;
-	    				String rexp = (rop!='#') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
-	    				curset.put(ans, new SubEvaluation('/', lexp+'/'+rexp));
-	    			}
-    			}
-    			if (l != 0) {
-	    			ans = r / l;
-	    			if (curset.get(ans) == null) {
-	    				char lop = entryright.getValue().operator;
-	    				String lexp = (lop=='+' || lop=='-') ? '('+entryright.getValue().expression+')' : entryright.getValue().expression;
-	    				char rop = entryleft.getValue().operator;
-	    				String rexp = (rop!='#') ? '('+entryleft.getValue().expression+')' : entryleft.getValue().expression;
-	    				curset.put(ans, new SubEvaluation('/', lexp+'/'+rexp));
-	    			}
+    			if ((left.operator=='*'||left.operator=='#') && (right.operator=='*'||right.operator=='#')) {
+        			v = left.val*right.val;
+        			max = lmax * (v==0?1:v) * rmax;
+        			max = Math.max(max, lmax + (v==0?1:v) * rmax);
+        			max = Math.max(max, lmax * (v==0?1:v) + rmax);
+        			max = Math.max(max, lmax + (v==0?1:v) + rmax);
+        			if (v<=Integer.MAX_VALUE && max>=target && -lmax-v-rmax<=target)
+        				curr.add(new SubEvaluation((int)v, '*', left.expression+'*'+right.expression));
     			}
     		}
     	}
@@ -155,8 +141,16 @@ public class ExpressionAddOperators
 	public static void main(String[] args)
 	{
 		ExpressionAddOperators solution = new ExpressionAddOperators();
-		
-		System.out.println(solution.card24(new int[]{12,5,7,6,11}));
+
+		System.out.println(solution.addOperators("10001",2));
+		System.out.println(solution.addOperators("2147483647", 2147483647));
+		System.out.println(solution.addOperators("999999999",80));
+		System.out.println(solution.addOperators("123", 6));
+		System.out.println(solution.addOperators("232", 8));
+		System.out.println(solution.addOperators("105", 5));
+		System.out.println(solution.addOperators("00", 0));
+		System.out.println(solution.addOperators("3456237490", 9191));
+		System.out.println(solution.addOperators("123456789", 45));
 	}
 
 }
