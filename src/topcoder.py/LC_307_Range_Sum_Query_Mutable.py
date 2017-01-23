b3b43887efabd569598b34252593f05ca9ea697c
Chauncey Chen <xicen@microsoft.com>
2017-01-23 16:49:48 +0800

    done

--- src/topcoder.py/LC_307_Range_Sum_Query_Mutable.py
+++ src/topcoder.py/LC_307_Range_Sum_Query_Mutable.py
@@ -19,7 +19,9 @@ Subscribe to see which companies asked this question
 Hide Tags Segment Tree Binary Indexed Tree
 Hide Similar Problems (E) Range Sum Query - Immutable (H) Range Sum Query 2D - Mutable
 
+Beat 98.91%
 @author: Chauncey
+
 '''
 
 import sys
@@ -33,11 +35,17 @@ class NumArray(object):
         if nums is None or len(nums) == 0:
             return
 
-        self.tree = [[0].extend(nums)]
-        sum = 0
-        for num in nums:
-            sum += num
-            self._leftsum.append(sum)
+        self.tree = [nums]
+        cur_nodes = nums
+        while len(cur_nodes) > 1:
+            next_nodes = []
+            for i in xrange(0, len(cur_nodes), 2):
+                left = cur_nodes[i]
+                right = 0 if i+1 >= len(cur_nodes) else cur_nodes[i+1]
+                next_nodes.append(left + right)
+            cur_nodes = next_nodes
+            self.tree.append(cur_nodes)
+        #print self.tree
         
 
     def update(self, i, val):
@@ -46,12 +54,23 @@ class NumArray(object):
         :type val: int
         :rtype: void
         """
-        if self._leftsum is None or i > len(self._leftsum) - 2:
+        if self.tree is None or i >= len(self.tree[0]):
             return
 
-        plus = val - self._leftsum[i+1] + self._leftsum[i]
-        for i in xrange(i+1, len(self._leftsum)):
-            self._leftsum[i] = self._leftsum[i] + plus
+        plus = val - self.tree[0][i]
+        self.tree[0][i] = val
+        for h in xrange(1, len(self.tree)):
+            i >>= 1
+            self.tree[h][i] += plus
+
+
+    def _sumBefore(self, i):
+        sum = 0
+        for h in xrange(0, len(self.tree)):
+            if i & 1 == 1:
+                sum += self.tree[h][i-1]
+            i >>= 1
+        return sum
         
 
     def sumRange(self, i, j):
@@ -60,15 +79,16 @@ class NumArray(object):
         :type j: int
         :rtype: int
         """
-        if i > j or self._leftsum is None:
+        if i > j or self.tree is None:
             return 0
 
         if i < 0:
             i = 0
-        if j > len(self._leftsum) - 2:
-            j = len(self._leftsum) - 2
+        if j >= len(self.tree[0]):
+            j = len(self.tree[0]) - 1
 
-        return self._leftsum[j+1] - self._leftsum[i]
+        #print j+1, self._sumBefore(j+1), '-', i, self._sumBefore(i)
+        return self._sumBefore(j+1) - self._sumBefore(i)
         
 
 
@@ -85,8 +105,15 @@ if __name__ == '__main__':
     print obj.sumRange(2, 5)
     #-3
     print obj.sumRange(0, 5)
+    print
 
     obj = NumArray([1, 3, 5])
-    print obj.sumRange(0, 2)
+    print obj.sumRange(0, 2) #9
     obj.update(1, 2)
-    print obj.sumRange(0, 2)
+    print obj.sumRange(0, 2) #8
+    print
+
+    obj = NumArray([-1])
+    print obj.sumRange(0, 0) #-1
+    obj.update(0, 1)
+    print obj.sumRange(0, 0) #1
